2f4002ef86425529ff981700b7cdfa41ff03095d
Benjamin Sullins <bensullins1@gmail.com>
2017-02-05 13:45:39 -0700

    Updated notes Signed-off-by: Benjamin Sullins <bensullins1@gmail.com>

--- main.m
+++ main.m
@@ -56,36 +56,70 @@ colorbar;
 % Begin by generating a background based on a weighted Power Spectrum
 % Distribution (PSD) of a collection of reference frames. These reference
 % frames provide the cornerstone of the underlying clutter.
+% 
+% Notes: As we progress through the project, we will develop a better model
+% of what clutter really is. This would use the PSD of reference images
+% found online. For now, just generate a gaussian noise image using the
+% parameters in the init.
 [ clutterImage ] = genClutter( frameInfo, clutterInfo );
 
 %% Generate Target
+% The target can be classified by generic distributions. These will also be
+% developed as we  enhance our algorithms. For now, just generate a
+% gaussian target with the parameters above.
 [ targetImage ] = genTarget( frameInfo, targetInfo );
 
+% This loop creates a "video" like representation with each frame being
+% generated on the fly.
 for frameNumber = 1 : displayInfo.numFrames
     
     %% Generate Clutter
-    % Could also generate the clutter on the fly
+    % Clutter can be generated on the fly as well if we wanted it to be. If
+    % not, comment this section out.
     [ clutterImage ] = genClutter( frameInfo, clutterInfo );
 
     %% Add Target Movement
+    % This function adds movement to the target within in the frame. We can
+    % modify this function to add specific movement patterns which reflect
+    % that of an actual target. For now, just generate some random
+    % patterns.
     [ targetMovImage updatedLocations] = moveTarget( frameInfo, targetImage, targetLocationInfo );
 
     % Lowpass the location movements so it doesnt look so crazy
+    % Lowpassing the target locations can be done to give the targets a
+    % more "organic" movement quality to them.
     targetLocationInfo.x = updatedLocations.x;
     targetLocationInfo.y = updatedLocations.y;
     
     %% Frame Combining
+    % Adding all the frames correctly is a little more complicated than
+    % what's shown. Nothing too bad, but for now just add away.
     finalImage = clutterImage + targetMovImage;
 
     %% Target Analysis
+    % This would be the meat and potatoes. We'd be taking a stab at
+    % understanding the underlying distributions for the target.
+    % Effectively, we'd be disseminating between what is or isn't a target
+    % (binomial for now). We can enhance this as we progress.
 
 
     %% Target Tracking
+    % Next, after finding the target, we'd like to understand what kind of
+    % offset is attributed to the targets motion from the previous frame.
+    % This offset tells the next frame where to place our location
+    % estimates to perform "tracking".
 
 
     %% Future Target Location Estimation
+    % Temporal estimation techniques (extended kalman filters) can be used
+    % to improve the tracking performance.
 
     %% Display Image
+    % Finally, just display the image so that the user has some
+    % understanding of what is going on. Im invisioning having a "truth"
+    % and an "actual" target location to help give some quality metrics on
+    % how we are performing. We can also throw in some other goodness if we
+    % want.
     
     % Update location for plotting
     plotlocation.x = [plotlocation.x, targetLocationInfo.x];
